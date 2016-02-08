713cef602aa1077e340499758dd628d4b416c4ba
kpeterkin101 <kswaggalaging@gmail.com>
2016-02-08 04:41:11 -0500

    Recent changes made to CSIS3030 assignment 6
    Due to the instruction we were given in class, these are the necessary
    files and changes made to the ecommerce database.

---
+++ part1.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (category_name)
+VALUES ('PC','Console');
+
+INSERT INTO products (category_id,product_name,price,quanity_remaining,description,image)
+VALUES ('1','FNAF','4.99','4','Welcome to your new summer job at Freddy Fazbears Pizza, where kids and parents alike come for entertainment and food! The main attraction is Freddy Fazbear, of course; and his two friends. They are animatronic robots, programmed to please the crowds!','fnaf.png');
+('1','Undertale','4.99','1','UNDERTALE! The RPG game where you dont have to destroy anyone.' 'undertale.png');
+('2','Mortal Kombat X','59.99','10','Mortal Kombat X is a fighting video game developed by NetherRealm Studios and published by Warner Bros. Interactive Entertainment.' 'mortal.jpg');
+('2','The Wolf Among Us','29.99','10','The Wolf Among Us is an episodic interactive mystery drama graphic adventure video game based on The Fables comic book series.' 'wolf.jpg');
